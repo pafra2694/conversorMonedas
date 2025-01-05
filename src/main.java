11c5e9ed3d27e42fc78b92dad89d74a48c994753
pafra2694 <pf.cantuvillanueva@outlook.com>
2025-01-05 14:55:00 -0600

    inicializando repositorio

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+
+        String opcion = null;
+
+        while (!opcion.equals("0")){
+            System.out.println("******CONVERSOR DE MONEDAS******\n\n" +
+                    "1. ");
+        }
+
+    }
+}
